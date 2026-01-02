d47a6c098e111a710d540de83f1f2b5b5a72acfd
Hieu3z03 <hieukdgvt@gmail.com>
2026-01-02 21:34:35 +0700

    feat: how to use lombok and mapstruct, bcryto

---
+++ src/main/java/com/example/jpa_crud/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package com.example.jpa_crud.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+import com.example.jpa_crud.dto.request.UserCreationRequest;
+import com.example.jpa_crud.dto.request.UserUpdateRequest;
+import com.example.jpa_crud.dto.response.UserResponse;
+import com.example.jpa_crud.entity.User;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    User toUser(UserCreationRequest request);
+
+    UserResponse toUserResponse(User user);
+
+    void updateUser(UserUpdateRequest request, @MappingTarget User user);
+}
