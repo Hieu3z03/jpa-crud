dda3d093d72edd05b62f00e9094dc7a3ac4a597a
Hieu3z03 <hieukdgvt@gmail.com>
2026-01-03 09:17:43 +0700

    feat: learning how to use jwt word in java

--- src/main/java/com/example/jpa_crud/controller/AuthenticationController.java
+++ src/main/java/com/example/jpa_crud/controller/AuthenticationController.java
@@ -2,11 +2,16 @@ package com.example.jpa_crud.controller;
 
 import com.example.jpa_crud.dto.request.ApiResponse;
 import com.example.jpa_crud.dto.request.AuthenticationRequest;
+import com.example.jpa_crud.dto.request.IntrospectRequest;
 import com.example.jpa_crud.dto.response.AuthenticationResponse;
+import com.example.jpa_crud.dto.response.IntrospectResponse;
 import com.example.jpa_crud.service.AuthenticationService;
+import com.nimbusds.jose.JOSEException;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.web.bind.annotation.*;
+
+import java.text.ParseException;
 
 @RestController
 @RequestMapping("/auth")
@@ -18,11 +23,17 @@ public class AuthenticationController {
 
     @PostMapping("/login")
     ApiResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
-        boolean result = authenticationService.authenticate(request);
+        var result = authenticationService.authenticate(request);
         return ApiResponse.<AuthenticationResponse>builder()
-                .result(AuthenticationResponse.builder()
-                        .authenticated(result)
-                        .build())
+                .result(result)
+                .build();
+    }
+
+    @PostMapping("/introspect")
+    ApiResponse<IntrospectResponse> introspect(@RequestBody IntrospectRequest request) throws ParseException, JOSEException {
+        var result = authenticationService.introspect(request);
+        return ApiResponse.<IntrospectResponse>builder()
+                .result(result)
                 .build();
     }
 }
--- src/main/java/com/example/jpa_crud/service/AuthenticationService.java
+++ src/main/java/com/example/jpa_crud/service/AuthenticationService.java
@@ -1,24 +1,98 @@
 package com.example.jpa_crud.service;
 
 import com.example.jpa_crud.dto.request.AuthenticationRequest;
+import com.example.jpa_crud.dto.request.IntrospectRequest;
+import com.example.jpa_crud.dto.response.AuthenticationResponse;
+import com.example.jpa_crud.dto.response.IntrospectResponse;
 import com.example.jpa_crud.exception.AppException;
 import com.example.jpa_crud.exception.ErrorCode;
 import com.example.jpa_crud.repository.UserRepository;
+import com.nimbusds.jose.*;
+import com.nimbusds.jose.crypto.MACSigner;
+import com.nimbusds.jose.crypto.MACVerifier;
+import com.nimbusds.jwt.JWTClaimsSet;
+import com.nimbusds.jwt.SignedJWT;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.*;
 
+import java.text.ParseException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+
 @Service
 @RequiredArgsConstructor
+@Slf4j
 @FieldDefaults(level = lombok.AccessLevel.PRIVATE, makeFinal = true)
 public class AuthenticationService {
     UserRepository userRepository;
 
-    public boolean authenticate(AuthenticationRequest request) {
-        var user = userRepository.findByUsername(request.getUsername()).orElseThrow(()-> new AppException(ErrorCode.USER_NOT_EXISTED));
+    @NonFinal
+    @Value("${jwt.signing-key}")
+    protected String SIGNING_SECRET;
+
+    public IntrospectResponse introspect(IntrospectRequest request)
+            throws JOSEException, ParseException {
+        var token = request.getToken();
+
+        JWSVerifier verifier = new MACVerifier(SIGNING_SECRET.getBytes());
+
+        SignedJWT signedJWT = SignedJWT.parse(token);
+
+        Date expiryTime = signedJWT.getJWTClaimsSet().getExpirationTime();
+
+        var verifierd = signedJWT.verify(verifier);
+
+        return IntrospectResponse.builder()
+                .valid(verifierd && expiryTime.after(new Date()))
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
         PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
-        return passwordEncoder.matches(request.getPassword(), user.getPassword());
+        var user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_EXISTED));
+
+        boolean authenticate = passwordEncoder.matches(request.getPassword(), user.getPassword());
+
+        if (!authenticate) {
+            throw new AppException(ErrorCode.UNAUTHICATED);
+        }
+
+        var token = generateToken(request.getUsername());
+
+        return AuthenticationResponse.builder()
+                .token(token)
+                .authenticated(true)
+                .build();
+    }
+
+    private String generateToken(String username) {
+        JWSHeader header = new JWSHeader(JWSAlgorithm.HS512);
+        JWTClaimsSet claimSet = new JWTClaimsSet.Builder()
+                .subject(username)
+                .issuer("example.com")
+                .issueTime(new Date())
+                .expirationTime(new Date(Instant.now().plus(1, ChronoUnit.HOURS).toEpochMilli()))
+                .claim("custom-claim", "custom")
+                .build();
+
+        Payload payload = new Payload(claimSet.toJSONObject());
+
+        JWSObject jwsObject = new JWSObject(header, payload);
+
+        try {
+            jwsObject.sign(new MACSigner(SIGNING_SECRET.getBytes()));
+            return jwsObject.serialize();
+        } catch (JOSEException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
